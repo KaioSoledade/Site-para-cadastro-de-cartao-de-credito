692a2dc42ca84c5f1e0a20a1cafb74bed0b00013
Kaio Soledade <soledadekaio@gmail.com>
2023-06-13 19:49:19 -0300

    js funcinado corretamente, somente com o input 1

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var InputTipo1 = document.getElementById('InputTipo1');
 var cartaoNumber1 = document.querySelector('#cartaoNumber1')
 
 InputTipo1.addEventListener('focus', function() {
-    cartaoNumber1.classList.add('cartao1-em-uso');
-    cartaoNumber1.classList.remove('cartao1-sem-uso');
+    cartaoNumber1.classList.add('margin-esquerda');
+    cartaoNumber2.classList.add('margin-direita');
 });
 
 InputTipo1.addEventListener('blur', function() {
-    cartaoNumber1.classList.remove('cartao1-em-uso');
-    cartaoNumber1.classList.add('cartao1-sem-uso');
+    cartaoNumber1.classList.remove('margin-esquerda');
+    cartaoNumber2.classList.remove('margin-direita');
 });
