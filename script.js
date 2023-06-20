1744d21181dd564dbdac60ca993142b7419570f7
Kaio Soledade <soledadekaio@gmail.com>
2023-06-20 20:40:25 -0300

    modelo JS de formulario, porem primitivo

--- script.js
+++ script.js
@@ -102,57 +102,86 @@ function sucesso(){
     mensagem.classList.remove('mensagem-sucesso-no')
     mensagem.classList.add('mensagem-sucesso-show')
 }
-
 function submit() {
-    var input1 = document.querySelector('.InputTipo1').value;
-    var input1e1 = document.querySelector('.InputTipo1.1').value;
-    var input2 = document.querySelector('.InputTipo2').value;
-    var input2e1 = document.querySelector('.InputTipo2.1').value;
-    var input2e2 = document.querySelector('.InputTipo2.2').value;
-  
+    var input1 = document.querySelector('#InputTipo1').value;
+    var input1e1 = document.querySelector('#InputTipo1.1').value;
+    var input2 = document.querySelector('#InputTipo2').value;
+    var input2e1 = document.querySelector('#InputTipo2.1').value;
+    var input2e2 = document.querySelector('#InputTipo2.2').value;
+
     // Verificando cada input separadamente
     var erro = false;
-  
-    if (input1 !== "") {
-      document.querySelector('#erro1').classList.remove('sem-erro');
-      document.querySelector('#erro1').classList.add('erro');
-      erro = true;
+
+    if (input1 == "") {
+        document.querySelector('#erro1').textContent = 'Por favor, preencha este campo.';
+        document.querySelector('#erro1').classList.remove('sem-erro');
+        document.querySelector('#erro1').classList.add('erro');
+        erro = true;
+    } else {
+        document.querySelector('#erro1').textContent = '';
+        document.querySelector('#erro1').classList.remove('erro');
+        document.querySelector('#erro1').classList.add('sem-erro');
     }
-  
-    if (input1e1 !== "") {
-      document.querySelector('#erro1e1').classList.remove('sem-erro');
-      document.querySelector('#erro1e1').classList.add('erro');
-      erro = true;
+
+    if (input1e1 == "") {
+        document.querySelector('#erro1e1').textContent = 'Por favor, preencha este campo.';
+        document.querySelector('#erro1e1').classList.remove('sem-erro');
+        document.querySelector('#erro1e1').classList.add('erro');
+        erro = true;
+    } else {
+        document.querySelector('#erro1e1').textContent = '';
+        document.querySelector('#erro1e1').classList.remove('erro');
+        document.querySelector('#erro1e1').classList.add('sem-erro');
     }
-  
-    if (input2 !== "") {
-      document.querySelector('#erro2').classList.remove('sem-erro');
-      document.querySelector('#erro2').classList.add('erro');
-      erro = true;
+
+    if (input2 == "") {
+        document.querySelector('#erro2').textContent = 'Por favor, preencha este campo.';
+        document.querySelector('#erro2').classList.remove('sem-erro');
+        document.querySelector('#erro2').classList.add('erro');
+        erro = true;
+    } else {
+        document.querySelector('#erro2').textContent = '';
+        document.querySelector('#erro2').classList.remove('erro');
+        document.querySelector('#erro2').classList.add('sem-erro');
     }
-  
-    if (input2e1 !== "") {
-      document.querySelector('#erro2e1').classList.remove('sem-erro');
-      document.querySelector('#erro2e1').classList.add('erro');
-      erro = true;
+
+    if (input2e1 == "") {
+        document.querySelector('#erro2e1').textContent = 'Por favor, preencha este campo.';
+        document.querySelector('#erro2e1').classList.remove('sem-erro');
+        document.querySelector('#erro2e1').classList.add('erro');
+        erro = true;
+    } else {
+        document.querySelector('#erro2e1').textContent = '';
+        document.querySelector('#erro2e1').classList.remove('erro');
+        document.querySelector('#erro2e1').classList.add('sem-erro');
     }
-  
-    if (input2e2 !== "") {
-      document.querySelector('#erro2e2').classList.remove('sem-erro');
-      document.querySelector('#erro2e2').classList.add('erro');
-      erro = true;
+
+    if (input2e2 == "") {
+        document.querySelector('#erro2e2').textContent = 'Por favor, preencha este campo.';
+        document.querySelector('#erro2e2').classList.remove('sem-erro');
+        document.querySelector('#erro2e2').classList.add('erro');
+        erro = true;
+    } else {
+        document.querySelector('#erro2e2').textContent = '';
+        document.querySelector('#erro2e2').classList.remove('erro');
+        document.querySelector('#erro2e2').classList.add('sem-erro');
     }
-  
+
+    
+    // Repita o mesmo padrão para os outros campos de entrada e mensagens de erro
+
     // Verificando se há mensagens de inputs não preenchidos
     if (erro) {
-      console.log("Faltam respostas em alguns inputs.");
+        console.log("Faltam respostas em alguns inputs.");
     } else {
-      var mensagem = document.querySelector('#sucesso');
-  
-      mensagem.classList.remove('mensagem-sucesso-no');
-      mensagem.classList.add('mensagem-sucesso-show');
+        var mensagem = document.querySelector('#sucesso');
+
+        mensagem.classList.remove('mensagem-sucesso-no');
+        mensagem.classList.add('mensagem-sucesso-show');
     }
-  }
+}
+
+
   
 
 
@@ -163,4 +192,30 @@ function confirme(){
     mensagem.classList.add('mensagem-sucesso-no')
     
     location.reload()
+}
+
+
+
+
+
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate(){
+    if(campos[0].value.legth < 3){
+        setError(0);
+    }
+    else{
+        removeError(0)
+    }
 }
